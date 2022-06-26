bd965a22d5d9bb25d0540643fbf92d6759ce154b
Rahul Singh <76153862+Rahul14wiz@users.noreply.github.com>
2022-06-26 06:27:05 +0530

    Added author date time and badges for source

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,17 +3,19 @@ import React, { Component } from "react";
 export class NewsItem extends Component {
 
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props
+    let { title, description, imageUrl, newsUrl, author, date,source } = this.props
     return (
       <div className="my-3">
         <div className="card">
-          <img src={!imageUrl?"https://ak.picdn.net/shutterstock/videos/9335021/thumb/10.jpg" : imageUrl} alt="..." />
+        <span class="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>{source}</span>
+          <img src={!imageUrl ? "https://ak.picdn.net/shutterstock/videos/9335021/thumb/10.jpg" : imageUrl} alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">
               {description}
             </p>
-            <a href={newsUrl} target = "_blank" className="btn btn-sm btn-dark" rel="noreferrer">
+            <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+            <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark" rel="noreferrer">
               Read More
             </a>
           </div>
